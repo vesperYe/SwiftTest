857747331653bc4a67e8d0cda68442dc44fb727e
vesperYe <ye_vesper@qq.com>
2015-12-04 22:03:37 +0800

    my app tset

---
+++ VercationList/VercationList/VacationInfo.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  VercationList
+//
+//  Created by 叶郑龙 on 15/11/26.
+//  Copyright © 2015年 叶郑龙. All rights reserved.
+//
+
+import Foundation
+
+class VacationInfo{
+    var place:String = "";
+    var visited:Bool = false;
+}
---
+++ VercationList/VercationList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VercationList
+//
+//  Created by 叶郑龙 on 15/11/24.
+//  Copyright © 2015年 叶郑龙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
